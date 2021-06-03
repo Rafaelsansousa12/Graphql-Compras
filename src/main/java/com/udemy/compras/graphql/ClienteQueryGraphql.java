4f6457eb2c476c3f83e0985396b75768023aad05
rafaelsansousa12 <rafaelsansousa@hotmail.com>
2021-06-03 00:47:07 -0300

    create project

---
+++ src/main/java/com/udemy/compras/graphql/ClienteQueryGraphql.java
@@ -0,0 +1,33 @@
+package com.udemy.compras.graphql;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ClienteQueryGraphql implements GraphQLQueryResolver, GraphQLMutationResolver {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    public Cliente cliente(Long id){
+        return clienteService.findById(id);
+    }
+
+    public List<Cliente> clientes(){
+        return clienteService.findAll();
+    }
+
+    public Cliente saveCliente(Long id, String nome, String email){
+
+        Cliente cliente = new Cliente(id, nome, email);
+        return clienteService.save(cliente);
+    }
+
+    public Boolean deleteCliente(Long id){
+        return clienteService.deleteById(id);
+    }
+}
